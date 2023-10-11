d76e48ffffe3cca3f5f610c8e4d9c015e2b6f1a1
haythem <saoudi_haythem@yahoo.com>
2023-10-11 23:39:02 +0100

    second push

--- src/main/java/com/poly/gestioncatalogue5gr1/GestionCatalogue5Gr1Application.java
+++ src/main/java/com/poly/gestioncatalogue5gr1/GestionCatalogue5Gr1Application.java
@@ -14,8 +14,8 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 
-public class GestionCatalogue5Gr1Application implements  CommandLineRunner{
-    @Autowired
+public class GestionCatalogue5Gr1Application{// implements  CommandLineRunner{
+   /* @Autowired
     CategorieRepository categorieRepository;
     @Autowired
     ProduitRepository produitRepository;
@@ -23,14 +23,23 @@ public class GestionCatalogue5Gr1Application implements  CommandLineRunner{
     public static void main(String[] args) {
         SpringApplication.run(GestionCatalogue5Gr1Application.class, args);
 
-    }
+    }*/
 
 
-    @Override
+    /*@Override
     public void run(String... args) throws Exception {
+
         categorieRepository.save(new Categorie(null,"informatique",null));
         categorieRepository.save(Categorie.builder().nom("electronique").build());
         produitRepository.save(new Produit(null,"pc portable",2000,5,categorieRepository.findById(1L).get()));
 
+    }*/
+    CommandLineRunner commandLineRunner(ProduitRepository produitRepository,CategorieRepository categorieRepository)
+    {
+        return args -> {
+            categorieRepository.save(new Categorie(null,"informatique",null));
+            categorieRepository.save(Categorie.builder().nom("electronique").build());
+            produitRepository.save(new Produit(null,"pc portable",2000,5,categorieRepository.findById(1L).get()));
+        };
     }
 }
